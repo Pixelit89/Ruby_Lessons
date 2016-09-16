2950d9d2ee694f530530713cca88a894a47e38a6
Andrei <pixelit@mail.ru>
2016-09-16 13:21:09 +0300

    Add post handling

--- Lesson20/barbershop/barbershop.rb
+++ Lesson20/barbershop/barbershop.rb
@@ -3,3 +3,15 @@ require 'sinatra'
 get '/' do
 	erb :index
 end
+
+post '/' do
+	@name = params[:first_name]
+	@last_name = params[:last_name]
+	@date = params[:date]
+	@phone = params[:phone]
+
+	@title = 'Thank you'
+	@message = "Dear #{@name} #{@last_name}. We will be waiting you on #{@date}."
+
+	erb :message
+end
