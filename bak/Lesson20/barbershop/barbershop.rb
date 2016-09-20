adce1d3d18e14a46c054d2951b14d37a9b9dafa5
Andrei <pixelit@mail.ru>
2016-09-20 11:53:08 +0300

    Lesson 20

--- bak/Lesson20/barbershop/barbershop.rb
+++ bak/Lesson20/barbershop/barbershop.rb
@@ -4,6 +4,27 @@ get '/' do
 	erb :index
 end
 
+get '/admin' do
+	erb :admin
+end
+
+post '/admin' do
+	@login = params[:login]
+	@password = @params[:password]
+	if @login == 'admin' && @password == 'pass'
+		@users_list = File.open("./users.txt", "r")
+		erb :admin
+	else
+		@mess = 'Неверный логин и/или пароль'
+		erb :admin
+	end
+end
+
+post '/getList' do
+	
+	erb :getList
+end
+
 post '/' do
 	@name = params[:first_name]
 	@last_name = params[:last_name]
@@ -17,4 +38,7 @@ post '/' do
 	f.write "#{@name} #{@last_name}, #{@phone}, #{@date}\n"
 	f.close
 	erb :message
+end
+get '/admin' do
+
 end
